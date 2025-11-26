d60245304f4a886239ec523308af101c7398e278
Jafer <jafzlawyer@gmail.com>
2025-11-26 00:32:52 +0000

    Try fixing this error: `Runtime Error: Error: `TabsContent` must be used

--- src/components/editor/editor-column.tsx
+++ src/components/editor/editor-column.tsx
@@ -20,30 +20,30 @@ export default function EditorColumn({ state, dispatch }: EditorColumnProps) {
     <section 
       className="no-print relative flex flex-col bg-card border-r w-[45%] min-w-[450px]"
     >
-      <div className="flex items-center p-2 border-b">
-        <SidebarTrigger />
-        <Tabs defaultValue="details" className="flex flex-col flex-1 overflow-hidden ml-2">
-          <TabsList>
+      <Tabs defaultValue="details" className="flex flex-col flex-1 overflow-hidden">
+        <div className="flex items-center p-2 border-b">
+          <SidebarTrigger />
+          <TabsList className="ml-2 flex-1">
             <TabsTrigger value="details" className="flex-1">ራስጌ እና ተዋዋይ ወገኖች</TabsTrigger>
             <TabsTrigger value="pleading" className="flex-1">የክሱ ፍሬነገሮች</TabsTrigger>
             <TabsTrigger value="evidence" className="flex-1">ማስረጃዎች</TabsTrigger>
           </TabsList>
-        </Tabs>
-      </div>
-
-      <ScrollArea className="flex-1">
-        <div className="p-4">
-            <TabsContent value="details">
-              <HeaderPartiesTab state={state} dispatch={dispatch} />
-            </TabsContent>
-            <TabsContent value="pleading">
-              <FactsTab state={state} dispatch={dispatch} />
-            </TabsContent>
-            <TabsContent value="evidence">
-              <EvidenceTab state={state} dispatch={dispatch} />
-            </TabsContent>
         </div>
-      </ScrollArea>
+
+        <ScrollArea className="flex-1">
+          <div className="p-4">
+              <TabsContent value="details">
+                <HeaderPartiesTab state={state} dispatch={dispatch} />
+              </TabsContent>
+              <TabsContent value="pleading">
+                <FactsTab state={state} dispatch={dispatch} />
+              </TabsContent>
+              <TabsContent value="evidence">
+                <EvidenceTab state={state} dispatch={dispatch} />
+              </TabsContent>
+          </div>
+        </ScrollArea>
+      </Tabs>
     </section>
   );
 }

8869ec719de145aef6f2f288ee1e426d895dbdd8
jaja44-hub <jshukrala@gmail.com>
2026-01-19 08:08:43 +0000

    fix: remove non-existent Nyala font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Ethiopic:wght@100..900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Nyala&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
